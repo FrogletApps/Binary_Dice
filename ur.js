550e2c9f4fe7a550aac967d7cb5e3487b4e937ff
James Pearson <jap61@kent.ac.uk>
2018-08-21 07:21:15 +0100

    Made code more efficient
    Removed some unneccesary code and also improved comments

--- ur.js
+++ ur.js
@@ -1,28 +1,27 @@
 var diceResults = new Array();
 var diceSum;
-var test;
 
 //Generates a random number between 0 and 1 then rounds it to the nearest one
 function dice(){
     return Math.round(Math.random());
 }
 
-//get 4 dice rolls (0 to 3) and add them to the array (and print them)
+//Get 4 dice rolls (0 to 3) and add them to the array (and print them)
 function roll(){
-    //clear and reset
+    //Clear and reset
     document.getElementById("roll").innerHTML = "";
     document.getElementById("total").innerHTML = "";
     diceSum = 0;
 
+    //Iterate through and generate dice rolls
     for(i=0; i<=3; i++){
         diceResults[i] = dice();
         document.getElementById("roll").innerHTML += diceResults[i];
-        test += diceResults[i];
         //console.log(diceResults[i]);
+
+        //Add the rolls together
+        diceSum += diceResults[i];
     }
 
-    for(i=0; i<=3; i++){
-        diceSum = diceSum + diceResults[i]
-    }
     document.getElementById("total").innerHTML = "The total is:  " + diceSum;
 }
